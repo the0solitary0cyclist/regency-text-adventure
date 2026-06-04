2d5e7f5c68d7925c515adf8bea5bc9ff58f43116
Cara Pacifico <carapacifico@mac.mynetworksettings.com>
2026-06-04 07:10:16 -0400

    try with repo name

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/the-westmoor-game/',
+//   base: '/the-westmoor-game/',
+  base: '/regency-text-adventure/',
 });
